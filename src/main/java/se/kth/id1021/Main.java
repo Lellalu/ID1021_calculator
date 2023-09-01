f370f99cdcd96a03e4cb7bb2ea01d6ead4095abd
Lellalu <lella.lu14@outlook.com>
2023-09-01 22:56:08 +0200

    Final version

--- src/main/java/se/kth/id1021/Main.java
+++ src/main/java/se/kth/id1021/Main.java
@@ -6,23 +6,74 @@ package se.kth.id1021;
  */
 public class Main{
     public static void main(String[] args) {
+        System.out.println("Adding " + args[0] + " integer to static and dynamic array.");
+        System.out.println("Running it " + args[1] + " times.");
 
-        int itemToAdd = 1000;
 
-        Stack s = new StaticStack(itemToAdd+1);
-        long startTime = System.nanoTime();
-        for(int i=0; i<itemToAdd; i++){
-            s.push(i);
+        int itemToAdd = Integer.parseInt(args[0]);
+        int maxLoop = Integer.parseInt(args[1]);
+
+        Stack s;
+        long startTimeStatic;
+        long endTimeStatic;
+        long startTimeDynamic;
+        long endTimeDynamic;
+        long[] staticRunningTime = new long[maxLoop];
+        long[] dynamicRunningTime = new long[maxLoop];
+
+        for(int i=0; i<maxLoop; i++){
+            s = new StaticStack(itemToAdd);
+            startTimeStatic = System.nanoTime();
+            for(int j=0; j<itemToAdd; j++){
+                s.push(j);
+            }
+            for(int j=0; j<itemToAdd; j++){
+                s.pop();
+            }
+            endTimeStatic = System.nanoTime();
+            staticRunningTime[i] = endTimeStatic-startTimeStatic;
+
+            s = new DynamicStack(4);
+            startTimeDynamic = System.nanoTime();
+            for(int j=0; j<itemToAdd; j++){
+                s.push(j);
+            }
+            for(int j=0; j<itemToAdd; j++){
+                s.pop();
+            }
+            endTimeDynamic = System.nanoTime();
+            dynamicRunningTime[i] = endTimeDynamic-startTimeDynamic;
         }
-        long endTime = System.nanoTime();
-        System.out.println("Static stack took: "+ Long.toString(endTime-startTime) + " ns.");
 
-        s = new DynamicStack(4);
-        startTime = System.nanoTime();
-        for(int i=0; i<itemToAdd; i++){
-            s.push(i);
+        long staticMaxTime = Long.MIN_VALUE;
+        long staticMintime = Long.MAX_VALUE;
+        long staticSumTime = 0;
+        long dynamicMaxTime = Long.MIN_VALUE;
+        long dynamicMinTime = Long.MAX_VALUE;
+        long dynamicSumTime = 0;
+        for(int i=0; i<maxLoop; i++){
+            staticSumTime += staticRunningTime[i];
+            dynamicSumTime += dynamicRunningTime[i];
+            if(staticRunningTime[i] > staticMaxTime) {
+                staticMaxTime = staticRunningTime[i];
+            }
+            if(dynamicRunningTime[i] > dynamicMaxTime) {
+                dynamicMaxTime = dynamicRunningTime[i];
+            }
+            if(staticRunningTime[i] < staticMintime) {
+                staticMintime = staticRunningTime[i];
+            }
+            if(dynamicRunningTime[i] < dynamicMinTime) {
+                dynamicMinTime = dynamicRunningTime[i];
+            }
         }
-        endTime = System.nanoTime();
-        System.out.println("Dynamic stack took: "+ Long.toString(endTime-startTime) + " ns.");
+
+        System.out.printf("Max StaticArray running time: %d ns\n", staticMaxTime);
+        System.out.printf("Min StaticArray running time: %d ns\n", staticMintime);
+        System.out.printf("StaticArray average running time: %.2f\n", (double) staticSumTime / maxLoop);
+        System.out.printf("Max DynamicArray running time: %d ns\n", dynamicMaxTime);
+        System.out.printf("Min DynamicArray running time: %d ns\n", dynamicMinTime);
+        System.out.printf("DynamicArray average running time: %.2f ns\n", (double) dynamicSumTime / maxLoop);
+        System.out.printf("ratio: %.2f\n", ((double) staticSumTime / maxLoop)/((double) dynamicSumTime / maxLoop));
     }
 }
--- src/main/java/se/kth/id1021/StaticStack.java
+++ src/main/java/se/kth/id1021/StaticStack.java
@@ -1,5 +1,12 @@
 package se.kth.id1021;
 
+
+/**
+* The static stack class extends the Stack abstract class, which  
+* constructs stacks that have fixed size.
+* throws StackFullException.
+* throws StackEmptyException.
+*/
 public class StaticStack extends Stack {
     public class StackFullException extends RuntimeException {}
     public class StackEmptyException extends RuntimeException {}
@@ -14,6 +21,11 @@ public class StaticStack extends Stack {
         this.stackPointer = -1;
     }
 
+    /**
+    * Push the value in stack.
+    * @throws StackFullException when the stack is full.
+    * @param i int value to put into the stack.
+    */
     public void push(int i) {
         if(stackPointer > size-2){
             throw new StackFullException();
@@ -23,6 +35,11 @@ public class StaticStack extends Stack {
         valueStack[stackPointer] = i;
     }
 
+    /**
+    * Pop the value on top out of in stack.
+    * @throws StackEmptyException when the stack is empty.
+    * @return the int value on top out of the stack.
+    */
     public int pop() {
         if(stackPointer == -1){
             throw new StackEmptyException();
